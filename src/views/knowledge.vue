5ac09b87d9ea8277a16f5c63b719830bfdfbcf76
zhangkai <815465599@qq.com>
2026-04-27 23:15:19 +0800

    知识文章下拉框选项获取

--- src/views/knowledge.vue
+++ src/views/knowledge.vue
@@ -15,15 +15,21 @@
 <script setup>
 import PageHead from '@/components/PageHead.vue'
 import TableSearch from '@/components/TableSearch.vue'
+import { onMounted,reactive, ref } from 'vue'
+import { categoryTree, articlePage } from '@/api/login'
 
 
 const formItem = [
   {comp : 'input', prop: 'title', label: '文章标题', placeholder: '请输入文章标题'},
-  {comp : 'select', prop: 'category', label: '分类', placeholder: '请选择分类', options: [{
-    label: '心理健康基础',
+  {comp : 'select', prop: 'category', label: '分类', placeholder: '请选择分类'},
+  {comp : 'select', prop: 'status', label: '状态', placeholder: '请选择状态', options: [{
+    label: '草稿',
+    value: '0'
+  },{
+    label: '已发布',
     value: '1'
   },{
-    label: '情绪管理',
+    label: '已下线',
     value: '2'
   }]}
 ]
@@ -31,5 +37,21 @@ const formItem = [
 const handleSearch = (formData) => {
   console.log(formData)
 }
+// 分类映射
+const categoryMap = reactive({})
+// 分类处理
+const categories = ref([])
+
+onMounted(async () => {
+  const data = await categoryTree()
+  categories.value = data.map(item => {
+    categoryMap[item.id] = item.categoryName
+    return {
+      label: item.categoryName,
+      value: item.id
+    }
+  })
+  formItem[1].options = categories.value
+})
 
 </script>
